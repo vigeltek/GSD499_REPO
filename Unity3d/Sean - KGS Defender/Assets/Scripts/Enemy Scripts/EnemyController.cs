f578cb8d72412099b7b6521d87de22d3e22a4627
SeanRChristie <SeanRChristie@gmail.com>
2015-10-04 11:37:52 -0400

    Signed-off-by: SeanRChristie <SeanRChristie@gmail.com>

--- Unity3d/Sean - KGS Defender/Assets/Scripts/Enemy Scripts/EnemyController.cs
+++ Unity3d/Sean - KGS Defender/Assets/Scripts/Enemy Scripts/EnemyController.cs
@@ -3,30 +3,33 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour 
 {
-	public GameObject enemyType;
-    public float spawnTimer = 3f;
-    public Transform[] spawnPoints;
-    public Transform[] targets;
 
-    public float healthPoints;
-    public float attackPower;
-    public float attackSpeed;
-    public float moveSpeed;
-    public float resourceValue;
-    public float waveModifier;
+
+
+
+    private int enemyCount;
+
 
     // Use this for initialization
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", spawnTimer, spawnTimer);
+        enemyCount = 0;
+    }
+
+    void Update()
+    {
+        
     }
 
     // Update is called once per frame
     void SpawnEnemy()
     {
-        int index = Random.Range(0, spawnPoints.Length);
-        GameObject clone = (GameObject)Instantiate(enemyType, spawnPoints[index].position, spawnPoints[index].rotation);
-        clone.GetComponent<MoveToTarget>().target = targets[index];
-        clone.GetComponent<NavMeshAgent>().speed = moveSpeed;
+        //int index = Random.Range(0, spawnPoints.Length);
+        //GameObject clone = (GameObject)Instantiate(enemyType, spawnPoints[index].position, spawnPoints[index].rotation);
+        //clone.GetComponent<MoveToTarget>().target = targets[index];
+        //clone.GetComponent<NavMeshAgent>().speed = moveSpeed;
+
+        //enemyCount++;
+        //currentEnemies[enemyCount] = clone;
     }
 }
