8eb211754155f1f5e400477d54790dee9d26f465
Kallan Dahn <kallandahn@gmail.com>
2015-10-06 18:20:24 -0400

    Turret targetting, target selection, and integration to the weapon system. Change to the stat script to go on enemies, and identified the tag "Player" that needs to go on each turret, and projectile to keep them from colliding with themselves. Have not turned on physics collision layer mask yet.

--- Unity3d/Sean - KGS Defender/Assets/Scripts/Stats.cs
+++ Unity3d/Sean - KGS Defender/Assets/Scripts/Stats.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Stats : MonoBehaviour {
 
     public float health;
-
+    WeaponController wc;
+    GameObject LastWeapon;
 
 	// Use this for initialization
 	void Start () {
@@ -16,13 +17,28 @@ public class Stats : MonoBehaviour {
 	
         if(health <= 0)
         {
+            if (LastWeapon != null)
+            {
+                wc = LastWeapon.GetComponent<WeaponController>();
+                wc.DeathConfirmation(this.gameObject);
+            }
+            //Instantiate death explosion
+
+            //Finally destroy this object.
             Destroy(this.gameObject);
         }
 
 	}
 
-    public void DamageObject(float dmg)
+    public void DamageObject(float dmg, GameObject parent)
     {
+        
+        LastWeapon = parent;
         health -= dmg;
+    }
+
+    public void ClearLastWeapon()
+    {
+        LastWeapon = null;
     }
 }
