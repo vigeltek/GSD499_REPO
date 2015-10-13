cf3a4d59082a974270fd6198b787c299e1c2d89c
George Vigelette <gvigelet@duvitech.com>
2015-10-12 21:03:23 -0400

    Main Menu update, need to wire up turret buttons

---
+++ Unity3d/KGS_Defender/Assets/Scripts/IntroMenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroMenuController : MonoBehaviour {
+
+    public string MainGameScene;
+
+    public void onStartGameClick()
+    {
+
+        Application.LoadLevel(MainGameScene);
+    }
+
+    public void onExitGameClick()
+    {
+        Application.Quit();
+    }
+
+    public void onCreditsClick()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
