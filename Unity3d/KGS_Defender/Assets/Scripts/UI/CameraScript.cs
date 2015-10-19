98e692a9934704c734dc4c11eabec36f756016f3
George Vigelette <gvigelet@duvitech.com>
2015-10-19 03:02:45 -0400

    added tooltips on hover, need to enable keys for tool tips yet.  zoom in and out works, pan left and right or forward and back by moving cursor to edges of screen.  Rotate camera with right mouse button held down.  middle button reset camera view.

--- Unity3d/KGS_Defender/Assets/Scripts/UI/CameraScript.cs
+++ Unity3d/KGS_Defender/Assets/Scripts/UI/CameraScript.cs
@@ -9,18 +9,23 @@ public class CameraScript : MonoBehaviour {
     public float bottomLimit = 0;
     public float leftLimit = 0;
     public float rightLimit = 0;
-
-    private float CurrentOrthoSize = 125;
-    private float MaxToClamp = 245;
-    private float MinToClamp = 75;
-    private float ROTSpeed = 10;
+    public int movementMargin = 35;
+    private float maxZoom = 35;
+    private float minZoom = 3;
+    private float ROTSpeed = 30;
     public float sensitivityX = 3.0f;
     public float sensitivityY = 3.0f;
-    private float mouseX;
-    private float mouseY;
+
+    private bool bRotating = false;
+    private float xRot = 0;
+    private float yRot = 0;
+    private float yaw = 0;
+    private float pitch = 0;
+    //private float mouseX;
+    //private float mouseY;
     private Vector3 cameraPos;
     private bool bDragging = false;
-
+    private Quaternion origRotation;
     // Use this for initialization
     void Start()
     {
@@ -30,13 +35,108 @@ public class CameraScript : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("X: " + _mainCamera.transform.position.x + " Y: " + _mainCamera.transform.position.y + " Z: " + _mainCamera.transform.position.z);
+        int scrWidth = Screen.width;
+        int scrHeight = Screen.height;
+        float newX = 0;
+        float newY = 0;
+        float newZ = 0;
 
-        if (bDragging)
+        //Debug.Log("X: " + _mainCamera.transform.position.x + " Y: " + _mainCamera.transform.position.y + " Z: " + _mainCamera.transform.position.z);
+        /*
+        mouseX = Input.mousePosition.x;
+        mouseY = Input.mousePosition.y;
+
+        Debug.Log("X: " + mouseX + " Y: " + mouseY);
+        */
+        if (Input.GetMouseButtonDown(2))
         {
-            mouseX = Input.GetAxis("Mouse X");
-            mouseY = Input.GetAxis("Mouse Y");
-            cameraPos = new Vector3(-mouseX * sensitivityX, 0, -mouseY * sensitivityY);
+            // zero camera
+            _mainCamera.transform.rotation = origRotation;
+            yaw = 0;
+            pitch = 0;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            // rotate camera enabled
+            //Debug.Log("Rotating ");
+            if (!bRotating)
+            {
+                bRotating = true;
+                xRot = Input.mousePosition.x;
+                yRot = Input.mousePosition.y;
+            }
+        }
+        else
+        {
+            // rotate camera disabled
+            if (bRotating)
+                bRotating = false;
+        }
+
+        if (bRotating)
+        {
+            if (Input.mousePosition.x > xRot + 5)
+            {
+                yaw += .25f;
+                if (yaw > 22.5f)
+                    yaw = 22.5f;
+                else
+                    _mainCamera.transform.Rotate(Vector3.up, .25f);
+
+            }else if(Input.mousePosition.x < xRot - 5)
+            {
+
+                yaw -= .25f;
+                if (yaw < -22.5f)
+                    yaw = -22.5f;
+                else
+                    _mainCamera.transform.Rotate(Vector3.up, -.25f);
+            }
+
+
+            if (Input.mousePosition.y > yRot + 5)
+            {
+                pitch += .25f;
+                if (pitch >= 22.5f)
+                    pitch = 22.5f;
+                else
+                    _mainCamera.transform.Rotate(Vector3.left, .25f);
+            }
+            else if (Input.mousePosition.y < yRot - 5)
+            {
+
+                pitch -= .25f;
+                if (pitch < -22.5f)
+                    pitch = -22.5f;
+                else
+                    _mainCamera.transform.Rotate(Vector3.left, -.25f);
+            }
+
+        }
+        else
+        {
+
+
+            if (Input.mousePosition.x >= 0 && Input.mousePosition.x <= movementMargin)
+            {
+                newX -= sensitivityX;
+            }
+            else if (Input.mousePosition.x >= scrWidth - movementMargin)
+            {
+                newX += sensitivityX;
+            }
+
+            if (Input.mousePosition.y >= 0 && Input.mousePosition.y <= movementMargin)
+            {
+                newY -= sensitivityY;
+            }
+            else if (Input.mousePosition.y >= scrHeight - movementMargin)
+            {
+                newY += sensitivityY;
+            }
+
+            cameraPos = new Vector3(newX, 0, newY);
 
             _mainCamera.transform.position += cameraPos;
             if (_mainCamera.transform.position.x > rightLimit)
@@ -56,42 +156,37 @@ public class CameraScript : MonoBehaviour {
             {
                 _mainCamera.transform.position = new Vector3(_mainCamera.transform.position.x, _mainCamera.transform.position.y, bottomLimit);
             }
-
         }
 
-        ZoomAmount += Input.GetAxis("Mouse ScrollWheel");
-        ZoomAmount = Mathf.Clamp(ZoomAmount, -MaxToClamp, MaxToClamp);
-        // Debug.Log("Zoom change: " + ZoomAmount);
-
-        CurrentOrthoSize += ZoomAmount;
-        if (CurrentOrthoSize < MinToClamp)
-            CurrentOrthoSize = MinToClamp;
-        if (CurrentOrthoSize > MaxToClamp)
-            CurrentOrthoSize = MaxToClamp;
-
-        _mainCamera.orthographicSize = CurrentOrthoSize;
-        if (Input.GetMouseButtonDown(2))
-        {
-            ZoomAmount = 0;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            bDragging = true;
-            // Debug.Log("Drag Start");
-        }
-        else if (Input.GetMouseButtonUp(0) && bDragging)
-        {
-            bDragging = false;
-            //  Debug.Log("Drag End");
-        }
-
+        _mainCamera.fieldOfView += (Input.GetAxis("Mouse ScrollWheel")*12);
+        
+        //Debug.Log("Zoom change: " + _mainCamera.fieldOfView);
+        if (_mainCamera.fieldOfView <= 8.0f)
+            _mainCamera.fieldOfView = 8.0f;
+        else if (_mainCamera.fieldOfView >= 40.0f)
+            _mainCamera.fieldOfView = 40.0f;
+        
+        //Debug.Log("FOV: " + _mainCamera.fieldOfView);
+        /*
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+                    bDragging = true;
+                    // Debug.Log("Drag Start");
+                }
+                else if (Input.GetMouseButtonUp(0) && bDragging)
+                {
+                    bDragging = false;
+                    //  Debug.Log("Drag End");
+                }
+        */
     }
 
     void Awake()
     {
         // Debug.Log("Got Orthographic camera");
         _mainCamera = GetComponent<Camera>();
-        _mainCamera.orthographicSize = CurrentOrthoSize;
+        //_mainCamera.orthographicSize = 26;
+        origRotation = _mainCamera.transform.rotation;
     }
 }
--- Unity3d/KGS_Defender/Assets/Scripts/UIController.cs
+++ Unity3d/KGS_Defender/Assets/Scripts/UIController.cs
@@ -1,17 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
     public GameObject score;
     public GameObject wave;
     public GameObject cash;
     GameManager GM;
-	// Use this for initialization
-	void Start ()
+
+    public GameObject laserButton;
+    public GameObject rocketButton;
+    public GameObject lightningButton;
+
+    public Text laserButtonToolTip;
+    public Text rocketButtonToolTip;
+    public Text lightningButtonToolTip;
+
+    // Use this for initialization
+    void Start ()
     {
         GM = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
+        
+    }
+
+    void Awake()
+    {
+        laserButtonToolTip.enabled = false;
+        rocketButtonToolTip.enabled = false;
+        lightningButtonToolTip.enabled = false;
     }
 	
 	// Update is called once per frame
@@ -19,5 +38,45 @@ public class UIController : MonoBehaviour {
     {
         cash.GetComponent<Text>().text = GM.GetCash().ToString();
         score.GetComponent<Text>().text = GM.GetScore().ToString();
+
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(eventData.hovered.Count>0)
+        {
+            foreach (GameObject g in eventData.hovered) {
+                if (g == laserButton) {
+                    // laserButtonToolTip.renderer.
+                    laserButtonToolTip.enabled = true;
+                }
+
+                if (g == rocketButton)
+                    rocketButtonToolTip.enabled = true;
+
+
+                if (g == lightningButton)
+                    lightningButtonToolTip.enabled = true;
+
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        foreach (GameObject g in eventData.hovered)
+        {
+            if (g == laserButton)
+                laserButtonToolTip.enabled = false;
+
+            if (g == rocketButton)
+                rocketButtonToolTip.enabled = false;
+
+
+            if (g == lightningButton)
+                lightningButtonToolTip.enabled = false;
+
+        }
     }
 }
