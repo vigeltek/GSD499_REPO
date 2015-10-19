627795724d28b84f7358fe1b1cd9d8fd7924d2d9
SeanRChristie <SeanRChristie@gmail.com>
2015-10-18 22:55:13 -0400

    Signed-off-by: SeanRChristie <SeanRChristie@gmail.com>

---
+++ Unity3d/KGS_Defender/Assets/PlayIntroMovie.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayIntroMovie : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        gameObject.GetComponent<AudioSource>().PlayDelayed(.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
