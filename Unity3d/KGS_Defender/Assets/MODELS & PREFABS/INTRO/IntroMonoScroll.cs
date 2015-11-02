bba1f231ff5da6e97adaaab469589d6373dc4f24
SeanRChristie <SeanRChristie@gmail.com>
2015-11-01 21:57:39 -0500

    Signed-off-by: SeanRChristie <SeanRChristie@gmail.com>

---
+++ Unity3d/KGS_Defender/Assets/MODELS & PREFABS/INTRO/IntroMonoScroll.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroMonoScroll : MonoBehaviour {
+
+    public TextAsset CreditsText;
+
+    public float Speed = 0.1f;
+
+    public int MaxFontSize = 32;
+
+    public GUIStyle TextStyle = new GUIStyle();
+
+    private GameObject creditHolder;
+
+    public Font TextFont;
+
+    private GUIText creditText;
+
+    private Transform creditHolderTransform;
+
+    private Animator anim;
+
+    private IntroVideoController IVC;
+
+    public void Start()
+    {
+        InitializeCreditHolder();
+        RecalculateFontSize();
+        PlaceCreditsAtTheScreensBottom();
+        anim = GameObject.FindGameObjectWithTag("IntroShip").GetComponent<Animator>();
+        IVC = GameObject.FindGameObjectWithTag("IntroVideoController").GetComponent<IntroVideoController>();
+    }
+
+    public void Update()
+    {
+        RecalculateFontSize();
+        MoveCreditsTextUntilEndIsReached();
+    }
+
+    private void InitializeCreditHolder()
+    {
+        creditHolder = new GameObject("Credits");
+        creditText = creditHolder.AddComponent<GUIText>();
+        creditText.font = TextFont;
+        
+        creditText.alignment = TextAlignment.Center;
+        creditText.anchor = TextAnchor.LowerCenter;
+        creditText.text = GetCreditsText();
+        creditText.fontStyle = TextStyle.fontStyle;
+        creditHolderTransform = creditHolder.transform;
+    }
+
+    void RecalculateFontSize()
+    {
+        int fontSize = MaxFontSize;
+        do
+        {
+            creditText.fontSize = fontSize;
+            fontSize--;
+        } while (creditText.GetScreenRect().width > Screen.width);
+    }
+
+    void PlaceCreditsAtTheScreensBottom()
+    {
+        float screeny = 0;
+        float y = 0.0f;
+        float minScreenY = (-1.0f * creditText.GetScreenRect().height) + Screen.height / 2;
+        do
+        {
+            creditHolderTransform.position = new Vector2(0.5f, y);
+            y -= 0.1f;
+
+            screeny = creditText.GetScreenRect().y;
+        } while (screeny > minScreenY);
+    }
+
+    private string GetCreditsText()
+    {
+        if (CreditsText != null)
+        {
+            return CreditsText.text;
+        }
+        return CreatePlaceHolderText();
+    }
+
+    private string CreatePlaceHolderText()
+    {
+        string placeHolderText = "These credits are only a placeholder\n\n\n";
+        for (int i = 0; i < 100; i++)
+        {
+            placeHolderText += "Please set 'CreditsText' text asset for real credits...\n";
+        }
+        placeHolderText += "\n\n\n\n\nThanks for watching placeholder credits this far :)";
+        return placeHolderText;
+    }
+
+    private void MoveCreditsTextUntilEndIsReached()
+    {
+        if (creditText.GetScreenRect().y > Screen.height * .99)
+        {
+            anim.SetBool("IsStart", true);
+            IVC.monoOver = true;
+            this.gameObject.SetActive(false);
+            return;
+        }
+        creditHolderTransform.Translate(Vector3.up * Time.deltaTime * Speed);
+    }
+}
---
+++ Unity3d/KGS_Defender/Assets/MODELS & PREFABS/INTRO/IntroVideoController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroVideoController : MonoBehaviour
+{
+    public bool monoOver;
+    public bool enterWarp;
+    public bool exitWarp;
+
+    public GameObject warpPS;
+    public GameObject warpSpeedFX;
+    public GameObject introShip;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(monoOver == true && enterWarp == false)
+        {
+            StartCoroutine(ShipIntro());
+
+            monoOver = false;
+            enterWarp = true;             
+        }
+        if(enterWarp = true && exitWarp == true)
+        {
+            StartCoroutine(EnterWarp());
+
+            enterWarp = false;
+            exitWarp = false;
+        }
+	}
+
+    IEnumerator ShipIntro()
+    {
+        warpPS.SetActive(true);
+        yield return new WaitForSeconds(2.75f);
+        warpPS.GetComponent<ParticleSystem>().Play(true);
+        yield return new WaitForSeconds(.25f);
+        introShip.SetActive(false);
+                
+        exitWarp = true;
+    }
+    IEnumerator EnterWarp()
+    {
+        yield return new WaitForSeconds(1.80f);
+        warpSpeedFX.SetActive(true);
+
+        gameObject.GetComponentInParent<AudioSource>().Stop();
+    }
+}
